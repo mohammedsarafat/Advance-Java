344069342e075262520ff40282a9a8747d6938f0
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-05-22 23:48:01 +0530

    Add Person2 entity with AadharCard relationship

---
+++ Hibernate/HibernateProjectDay03_One_to_one_mapping_02/com.nit.dto/Person2.java
@@ -0,0 +1,61 @@
+package com.nit.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Person2 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String address;
+	private long mob;
+	
+	@OneToOne
+	private AadharCard card;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getaddress() {
+		return address;
+	}
+	public long getMob() {
+		return mob;
+	}
+	
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setaddress(String address) {
+		this.address = address;
+	}
+	public void setMob(long mob) {
+		this.mob = mob;
+	}
+	
+	public AadharCard getCard() {
+		return card;
+	}
+	public void setCard(AadharCard card) {
+		this.card = card;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person2 [id=" + id + ", name=" + name + ", address=" + address + ", mob=" + mob + "]";
+	}
+		
+}
